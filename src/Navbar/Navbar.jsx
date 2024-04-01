751a7f79106999adf3f545a3d56982776c49ba7a
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-04-01 10:33:40 +0530

    tst

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './nav.css'
+const Navbar = () => {
+  return (
+    <div>
+      <nav className='Nav_Link_Nav' >
+        <div>
+            <h4>Book Us Now</h4>
+            </div>
+        <ul>
+            <li></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
+
