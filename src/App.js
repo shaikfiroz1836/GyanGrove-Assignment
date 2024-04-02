5f04a54cd8cad5cf9a22774eaa893453c1632e8e
shaikfiroz1836 <shaikfiroz1836@gmail.com>
2024-04-02 11:05:03 +0530

    tst

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./Navbar/Navbar";
-
+import Practice from "./Components/Practice";
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Practice/>
     </div>
   );
 }
